f20cae64869de0eaa6d758c4d96d0616a0e3ac98
Kirsi Trospe <ladm@DESKTOP-I23NN6G.(none)>
2024-09-16 17:58:51 +0200

    Add sinus function

--- LearnCpp.cpp
+++ LearnCpp.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include <iomanip>
+#include <math.h>
+
+void PrintSin(int number_of_steps)
+{
+    float x = 0;
+    const double PI = std::atan(1.0) * 4;
+    float dx = 2 * PI / number_of_steps;
+
+
+    std::cout << "\t"<< std::setw(10) << std::right << "x" << "\t" << std::setw(10) << std::right << "sin(x)" << std::endl;
+    for (int i = 0;i < number_of_steps;i++)
+    {
+        std::cout << "\t" << std::setw(10) << std::right << x << "\t" << std::setw(10) << std::right << std::sin(x) << std::endl;
+        x = x + dx;
+    }
+}
 
 int main()
 {
     int points[] = { 5,3,2 };
     std::string people[] = { "Hupu", "Tupu", "Lupu" };
     std::cout << "Hello " << people[0] << "," << std::endl;
-    std::cout << "You have " << points[0] << "points!" << std::endl <<std::endl;
+    std::cout << "You have " << points[0] << " points!" << std::endl <<std::endl;
 
     std::cout << "\tResults\n\n";
     std::cout << "\tName" << "\tPoints" << std::endl;
@@ -14,6 +31,12 @@ int main()
     {
         std::cout << "\t" << people[i] << "\t" << points[i] << std::endl;
     }
+
+    std::cout << "-------------------------------------------------------" << std::endl << std::endl;
+
+    int steps = 10;
+
+    PrintSin(steps);
 
     system("pause>0");
 }
