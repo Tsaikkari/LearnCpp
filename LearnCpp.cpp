d87e905b5650000f8e632e33f1ff440a38ccfb6b
Kirsi Trospe <ladm@DESKTOP-I23NN6G.(none)>
2024-09-17 20:23:16 +0200

    Change class

--- LearnCpp.cpp
+++ LearnCpp.cpp
@@ -2,12 +2,20 @@
 #include <iomanip>
 #include <math.h>
 
-#define NUMBER_OF_COLUMNS 3
 #define NUMBER_OF_ROWS 3
+
+class Header {
+	public:
+        std::string h0="id";
+        std::string h1="name";
+        std::string h2="points";
+};
 
 class Person {
     public:
-        std::string columns[NUMBER_OF_COLUMNS];
+        int id;
+        std::string name;
+        int points;
 };
 
 void PrintSin(int number_of_steps)
@@ -48,32 +56,34 @@ int main()
 
     std::cout << "-------------------------------------------------------" << std::endl << std::endl;
 
-    Person table[NUMBER_OF_ROWS];
-    int row;
+    Header header;
+   
+    Person persons[NUMBER_OF_ROWS];
 
-    row = 0; // header
-    table[row].columns[0] = "id";
-    table[row].columns[1] = "name";
-    table[row].columns[2] = "wert";
+    persons[0].id = 0;
+	persons[0].name = "Sven";
+	persons[0].points = 4;
 
-    row = 1;
-    table[row].columns[0] = "0";
-    table[row].columns[1] = "Max";
-    table[row].columns[2] = "5";
+    persons[1].id = 1;
+	persons[1].name = "Max";
+	persons[1].points = 5;
+	
+	persons[2].id = 2;
+	persons[2].name = "Peter";
+	persons[2].points = 1;
 
-    row = 2;
-    table[row].columns[0] = "1";
-    table[row].columns[1] = "Bert";
-    table[row].columns[2] = "3";
+    std::cout << "\t" <<header.h0;
+	std::cout << "\t" <<header.h1;
+	std::cout << "\t" <<header.h2;
+	std::cout << std::endl;
 
-    for (int row = 0; row < NUMBER_OF_ROWS; row++) {
-        for (int col = 0; col < NUMBER_OF_COLUMNS; col++) {
-            std::cout << "\t" << table[row].columns[col];
-        }
+    for (int r = 0; r < NUMBER_OF_ROWS; r++) {
+        std::cout << "\t"<< persons[r].id;
+        std::cout << "\t"<< persons[r].name;
+        std::cout << "\t"<< persons[r].points;
         std::cout << std::endl;
     }
     
-
     system("pause>0");
 }
 
